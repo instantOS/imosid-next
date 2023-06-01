9230cdbfb2bf5a0a44963870be6434b12647d07a
paperbenni <paperbenni@gmail.com>
2023-06-01 14:18:59 +0200

    some refactoring

--- src/comment.rs
+++ src/comment.rs
@@ -1,6 +1,5 @@
 use regex::Regex;
 
-
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub enum CommentType {
     SectionBegin,
@@ -16,7 +15,7 @@ pub struct Specialcomment {
     pub line: u32, // line number comment is at in file
     content: String,
     pub section: String, // section name extracted from prefix
-    pub ctype: CommentType, 
+    pub ctype: CommentType,
     pub argument: Option<String>, // optional argument, used for hashes etc
 }
 
@@ -34,118 +33,107 @@ impl Specialcomment {
 
         let keywords = commentregex.captures(&line);
 
-        match &keywords {
-            Some(captures) => {
-                let keywords = captures
-                    .get(1)
-                    .unwrap()
-                    .as_str()
-                    .split(" ")
-                    .collect::<Vec<&str>>();
+        if let Some(captures) = &keywords {
+            let keywords = captures
+                .get(1)
+                .unwrap()
+                .as_str()
+                .split(" ")
+                .collect::<Vec<&str>>();
 
-                // needs at least a section and a keyword
-                if keywords.len() < 2 {
-                    return Option::None;
+            // needs at least a section and a keyword
+            if keywords.len() < 2 {
+                return Option::None;
+            }
+
+            let sectionname = keywords[0];
+            let keyword = keywords[1];
+            //comment argument, example #...all source ARGUMENT
+            let cargument: Option<String> = if keywords.len() > 2 {
+                Option::Some(String::from(keywords[2]))
+            } else {
+                Option::None
+            };
+
+            let tmptype: CommentType;
+            match keyword {
+                "begin" | "start" => {
+                    tmptype = CommentType::SectionBegin;
                 }
-
-                let sectionname = keywords[0];
-                let keyword = keywords[1];
-                //comment argument, example #...all source ARGUMENT
-                let cargument: Option<String>;
-
-                if keywords.len() > 2 {
-                    cargument = Option::Some(String::from(keywords[2]));
-                } else {
-                    cargument = Option::None;
+                "end" | "stop" => {
+                    tmptype = CommentType::SectionEnd;
                 }
-
-                let tmptype: CommentType;
-                match keyword {
-                    "begin" | "start" => {
-                        tmptype = CommentType::SectionBegin;
+                "hash" => {
+                    tmptype = CommentType::HashInfo;
+                    if cargument == None {
+                        println!("missing hash value on line {}", linenumber);
+                        return Option::None;
                     }
-                    "end" | "stop" => {
-                        tmptype = CommentType::SectionEnd;
-                    }
-                    "hash" => {
-                        tmptype = CommentType::HashInfo;
-                        match cargument {
-                            Some(_) => {}
-                            None => {
-                                println!("missing hash value on line {}", linenumber);
-                                return Option::None;
-                            }
+                }
+                "source" => {
+                    tmptype = CommentType::SourceInfo;
+                    match cargument {
+                        Some(_) => {
+                            println!("updating from source not implemented yet");
+                            //TODO do something
+                            //fetch from file/url/git
                         }
-                    }
-                    "source" => {
-                        tmptype = CommentType::SourceInfo;
-                        match cargument {
-                            Some(_) => {
-                                println!("updating from source not implemented yet");
-                                //TODO do something
-                                //fetch from file/url/git
-                            }
-                            None => {
-                                println!("missing source file on line {}", linenumber);
-                                return Option::None;
-                            }
-                        }
-                    }
-                    "permissions" => {
-                        // permissioms can only be set for the entire file
-                        if sectionname != "all" {
-                            return Option::None;
-                        }
-                        match &cargument {
-                            None => {
-                                return Option::None;
-                            }
-                            Some(arg) => match arg.parse::<u32>() {
-                                Err(_) => {
-                                    return Option::None;
-                                }
-                                Ok(_) => {
-                                    tmptype = CommentType::PermissionInfo;
-                                }
-                            },
-                        }
-                    }
-                    "target" => {
-                        if sectionname == "all" {
-                            tmptype = CommentType::TargetInfo;
-                            match cargument {
-                                Some(_) => {}
-                                None => {
-                                    println!("missing target value on line {}", linenumber);
-                                    return Option::None;
-                                }
-                            }
-                        } else {
-                            println!(
-                                "warning: target can only apply to the whole file {}",
-                                linenumber
-                            );
+                        None => {
+                            println!("missing source file on line {}", linenumber);
                             return Option::None;
                         }
                     }
-
-                    &_ => {
-                        println!("warning: incomplete imosid comment on {}", linenumber);
+                }
+                "permissions" => {
+                    // permissioms can only be set for the entire file
+                    if sectionname != "all" {
+                        return Option::None;
+                    }
+                    match &cargument {
+                        None => {
+                            return Option::None;
+                        }
+                        //todo: more validation. maybe own permission type?
+                        Some(arg) => match arg.parse::<u32>() {
+                            Err(_) => {
+                                return Option::None;
+                            }
+                            Ok(_) => {
+                                tmptype = CommentType::PermissionInfo;
+                            }
+                        },
+                    }
+                }
+                "target" => {
+                    if sectionname == "all" {
+                        tmptype = CommentType::TargetInfo;
+                        if cargument == None {
+                            println!("missing target value on line {}", linenumber);
+                            return Option::None;
+                        }
+                    } else {
+                        println!(
+                            "warning: target can only apply to the whole file {}",
+                            linenumber
+                        );
                         return Option::None;
                     }
                 }
 
-                Option::Some(Specialcomment {
-                    line: linenumber,
-                    content: String::from(line),
-                    section: String::from(sectionname),
-                    ctype: tmptype,
-                    argument: cargument,
-                })
+                &_ => {
+                    println!("warning: incomplete imosid comment on {}", linenumber);
+                    return Option::None;
+                }
             }
-            None => {
-                return Option::None;
-            }
-        }
+
+            return Some(Specialcomment {
+                line: linenumber,
+                content: String::from(line),
+                section: String::from(sectionname),
+                ctype: tmptype,
+                argument: cargument,
+            });
+        };
+        return Option::None;
     }
 }
